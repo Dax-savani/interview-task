c07a63cbe4b38a5961af4899806a8c6354b320be
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-04-02 19:05:04 +0530

    set routes and make sign-in and sign-up page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Navigate, Routes, Route} from "react-router-dom";
+import SignIn from "../pages/sign-in/SignIn";
+import SignUp from "../pages/sign-up/SIgnUp";
+import Home from "../pages/home/Home";
+
+const PrivateRoute = ({children}) => {
+    const isAuthenticated = !!localStorage.getItem('authToken');
+    return isAuthenticated ? children : <Navigate to="/signin" replace/>;
+};
 
 function App(props) {
-    return (
-        <div>
 
-        </div>
+    return (
+        <>
+            <Routes>
+                <Route path="/sign-in" element={<SignIn/>}/>
+                <Route path="/sign-up" element={<SignUp/>}/>
+                <PrivateRoute>
+                    <Route
+                        path="/"
+                        element={
+                            <Home/>
+                        }
+                    />
+                </PrivateRoute>
+                <Route path="*" element={<Navigate to="/" replace/>}/>
+            </Routes>
+        </>
     );
 }
 
