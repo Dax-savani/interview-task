16c025f49614f482381693891f00f03fcf92a5a4
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-04-03 11:00:13 +0530

    create some ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {createContext, useEffect} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Navigate, Routes, Route} from "react-router-dom";
 import SignIn from "./pages/sign-in/SignIn";
 import SignUp from "./pages/sign-up/SIgnUp";
@@ -7,23 +7,36 @@ import PrivateRoute from "./components/auth/PrivateRoute";
 import {Toaster} from "react-hot-toast";
 import AdminPrivateRoute from "./components/auth/AdminPrivateRoute";
 import Admin from "./pages/admin/Admin";
-import meInstance from "./axiosInstance/meAxiosInstance";
+import {UserContext} from "./index";
+import axios from "axios";
 
 function App(props) {
+    const user = useContext(UserContext)
     const me = async () => {
-        const res = await meInstance.post(`/auth/refresh`)
-        console.log(res)
+        try {
+
+            const res = await axios.post(`https://interview-task-be-u1e1.onrender.com/v1/auth/refresh`, {refreshToken: user?.token?.refreshToken})
+            if (res.status == 200) {
+                const newTokens = { ...user, token: res.data };
+                localStorage.setItem("user", JSON.stringify(newTokens))
+                console.log(res.data)
+            }
+        } catch (e) {
+            console.log(e)
+        }
     }
     useEffect(() => {
-    me()
+        if (user?.email) {
+            me()
+        }
     }, []);
     return (
         <>
-            <Toaster position="top-center" reverseOrder={false} />
+            <Toaster position="top-center" reverseOrder={false}/>
             <Routes>
                 <Route path="/sign-in" element={<SignIn/>}/>
                 <Route path="/sign-up" element={<SignUp/>}/>
-                <Route element={<PrivateRoute />}>
+                <Route element={<PrivateRoute/>}>
                     <Route
                         path="/"
                         element={
@@ -31,7 +44,7 @@ function App(props) {
                         }
                     />
                 </Route>
-                <Route element={<AdminPrivateRoute />}>
+                <Route element={<AdminPrivateRoute/>}>
                     <Route
                         path="/admin"
                         element={
